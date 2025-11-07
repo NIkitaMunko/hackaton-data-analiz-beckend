a44e53cf68f5b1d24321a396bd707631793aa45d
Danylo Kliui <danylo.kliui@student.tuke.sk>
2025-11-07 20:17:00 +0100

    Add dataset import

--- llm/inference.py
+++ llm/inference.py
@@ -1,4 +1,9 @@
 import string
+from datasets import load_dataset
+
+DATA_PATH = "./dataset/receipts.csv"
+
+dataset = load_dataset("csv", data_files=DATA_PATH, split="train")
 
 # TODO: (конект к ендпоинтс)(Никита)
 def receiver(text : string) -> str:
